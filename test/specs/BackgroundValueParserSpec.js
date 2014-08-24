248023f2f82a97de4ceed4fd5869fdf16f9c4d18
Christoph Burgmer <christoph.burgmer@gmail.com>
2014-08-24 15:16:27 +0200

    Document shortcomming of background value parser

--- test/specs/BackgroundValueParserSpec.js
+++ test/specs/BackgroundValueParserSpec.js
@@ -56,4 +56,59 @@ describe("Background value parser", function () {
             }).toThrow(new Error("Invalid url"));
         });
     });
+
+    describe("parse", function () {
+        it("should parse a url", function () {
+            var values = backgroundValueParser.parse('url(path/file.png)');
+            expect(values).toEqual([{
+                preUrl: [],
+                url: 'path/file.png',
+                postUrl: []
+            }]);
+        });
+
+        it("should parse an empty string", function () {
+            var values = backgroundValueParser.parse('');
+            expect(values).toEqual([]);
+        });
+
+        // shortcomming of the background value parser
+        xit("should deal with a linear-gradient", function () {
+            var values = backgroundValueParser.parse('linear-gradient(to bottom right, red, rgba(255,0,0,0))');
+            expect(values).toEqual([{
+                preUrl: ['linear-gradient(to bottom right, red, rgba(255,0,0,0))']
+            }]);
+        });
+
+        it("should deal with a url mixed with a linear-gradient", function () {
+            var values = backgroundValueParser.parse('url(path/file.png), linear-gradient(to bottom right, red, rgba(255,0,0,0))');
+            expect(values[0]).toEqual({
+                preUrl: [],
+                url: 'path/file.png',
+                postUrl: []
+            });
+        });
+
+        it("should deal with a url mixed with a linear-gradient", function () {
+            var values = backgroundValueParser.parse('linear-gradient(to bottom right, red, rgba(255,0,0,0)), url(path/file.png)');
+            expect(values[values.length-1]).toEqual({
+                preUrl: [],
+                url: 'path/file.png',
+                postUrl: []
+            });
+        });
+    });
+
+    describe('back and force conversion', function () {
+        it("should handle a url", function () {
+            var cssValue = backgroundValueParser.serialize(backgroundValueParser.parse('url(path/file.png)'));
+            expect(cssValue).toEqual('url("path/file.png")');
+        });
+
+        it("should deal with a url mixed with a linear-gradient", function () {
+            var cssValue = backgroundValueParser.serialize(backgroundValueParser.parse('url(path/file.png), linear-gradient(to bottom right, red, rgba(255,0,0,0))'));
+            expect(cssValue).toEqual('url("path/file.png"), linear-gradient(to bottom right, red, rgba(255, 0, 0, 0))');
+        });
+
+    });
 });
