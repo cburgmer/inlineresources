121b2b364007052131566feed71415e083f358b1
Christoph Burgmer <christoph.burgmer@gmail.com>
2018-03-18 16:16:35 +0100

    Refactor, remove webserver, Chromium can load from file directly, contrary to PhantomJS before

--- examples/bundlePage.js
+++ examples/bundlePage.js
@@ -1,57 +1,16 @@
 #!/usr/bin/env node
 
-const http = require('http'),
-      url = require('url'),
-      fs = require('fs'),
-      path = require('path'),
+const path = require('path'),
       puppeteer = require('puppeteer');
 
-const port = 8000;
-
-const contentTypeMap = {
-    'html': 'text/html',
-    'htm': 'text/html',
-    'css': 'text/css',
-    'js': 'application/javascript'
-};
-
-const guessContentType = function (fileName) {
-    const fallback = 'text/plain';
-
-    if (fileName.lastIndexOf('.') < 1) {
-        return fallback;
-    }
-
-    const extension = fileName.substr(fileName.lastIndexOf('.') + 1);
-    return contentTypeMap[extension] || fallback;
-};
-
-const startWebserver = function () {
-    http.createServer((request, response) => {
-        const localPath = '.' + decodeURIComponent(url.parse(request.url).pathname);
-
-        if (fs.existsSync(localPath)) {
-            // Content-Type to work around https://bugzilla.mozilla.org/show_bug.cgi?id=942138
-            response.writeHead(200, {"Content-Type": guessContentType(localPath)});
-            response.write(fs.readFileSync(localPath, {flag: 'r'}));
-            response.end();
-        } else {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-        }
-
-    }).listen(port);
-};
-
-const getFileUrl = function (address) {
-    return address.indexOf("://") === -1 ? "http://localhost:" + port + "/" + address : address;
+const fileUrl = (relPath) => {
+    return 'file://' + path.resolve(process.cwd(), relPath);
 };
 
 const bundlePage = async (url) => {
-    const browser = await puppeteer.launch(),
+    const browser = await puppeteer.launch({args: ['--allow-file-access-from-files']}),
           page = await browser.newPage(),
-          pageUrl = getFileUrl(url);
+          pageUrl = fileUrl(url);
 
     page.on('console', msg => {
         for (let i = 0; i < msg.args().length; ++i) {
@@ -62,7 +21,7 @@ const bundlePage = async (url) => {
         console.error(msg);
     });
 
-    await page.goto(getFileUrl('examples/bundlePage.html'));
+    await page.goto(fileUrl('examples/bundlePage.html'));
 
     const xhtml = await page.evaluate(function (pageUrl) {
         return bundlePage(pageUrl);
@@ -81,7 +40,6 @@ const main = async () => {
 
     const pageToInline = process.argv[2];
 
-    startWebserver();
     await bundlePage(pageToInline);
 };
 
